d3699eb2acd12f900818ae7009ac2396ad88b781
buyaramadhan <ytchochote@gmail.com>
2025-06-06 21:30:33 +0700

    menambahkan repository untuk data anak

---
+++ lib/data/repository/anak_repository.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:canary_template/data/model/request/admin/anak_request_model.dart';
+import 'package:canary_template/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+
+class AnakRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  AnakRepository(this._serviceHttpClient);
+
+  Future<Either<String, GetAnakById>> addAnak(
+    AnakRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        "admin/anak",
+        requestModel.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetAnakById.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
+
+  Future<Either<String, GetAllAnakModel>> getAllAnak() async {
+    try {
+      final response = await _serviceHttpClient.get("admin/anak");
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetAllAnakModel.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while get all anak: $e");
+    }
+  }
+}
