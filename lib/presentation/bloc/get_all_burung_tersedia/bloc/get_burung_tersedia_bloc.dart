8285e035fd0c16b933ddb158464d4d2fe59882ab
buyaramadhan <ytchochote@gmail.com>
2025-06-06 22:53:27 +0700

    membuat bloc untuk all burung tersedia

---
+++ lib/presentation/bloc/get_all_burung_tersedia/bloc/get_burung_tersedia_bloc.dart
@@ -0,0 +1,30 @@
+
+import 'package:canary_template/data/model/response/burung_semua_tersedia.dart';
+import 'package:canary_template/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'get_burung_tersedia_event.dart';
+part 'get_burung_tersedia_state.dart';
+
+class GetBurungTersediaBloc
+    extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
+  final GetAllBurungTersediaRepository getAllBurungTersediaRepository;
+
+  GetBurungTersediaBloc(this.getAllBurungTersediaRepository)
+    : super(GetBurungTersediaInitial()) {
+    on<GetAllBurungTersediaEvent>(_getAllBurungTersedia);
+  }
+
+  Future<void> _getAllBurungTersedia(
+    GetAllBurungTersediaEvent event,
+    Emitter<GetBurungTersediaState> emit,
+  ) async {
+    emit(GetBurungTersediaLoading());
+    final result = await getAllBurungTersediaRepository.getAllBurungTersedia();
+    result.fold(
+      (error) => emit(GetBurungTersediaError(message: error)),
+      (burungTersedia) =>
+          emit(GetBurungTersediaLoaded(burungTersedia: burungTersedia)),
+    );
+  }
+}
---
+++ lib/presentation/bloc/get_all_burung_tersedia/bloc/get_burung_tersedia_state.dart
@@ -0,0 +1,17 @@
+part of 'get_burung_tersedia_bloc.dart';
+
+sealed class GetBurungTersediaState {}
+
+final class GetBurungTersediaInitial extends GetBurungTersediaState {}
+
+final class GetBurungTersediaLoading extends GetBurungTersediaState {}
+
+final class GetBurungTersediaLoaded extends GetBurungTersediaState {
+  final BurungSemuaTersediaModel burungTersedia;
+  GetBurungTersediaLoaded({required this.burungTersedia});
+}
+
+final class GetBurungTersediaError extends GetBurungTersediaState {
+  final String message;
+  GetBurungTersediaError({required this.message});
+}
