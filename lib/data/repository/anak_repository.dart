bfb56e93c15b438942333b8246c4716c1fcfc8fa
buyaramadhan <ytchochote@gmail.com>
2025-06-06 22:01:59 +0700

    menambahkan model GetIndukById dan GetAllIndukModel

--- lib/data/repository/anak_repository.dart
+++ lib/data/repository/anak_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:canary_template/data/model/request/admin/anak_request_model.dart';
+import 'package:canary_template/data/model/response/get_all_anak_response.dart';
 import 'package:canary_template/service/service_http_client.dart';
 import 'package:dartz/dartz.dart';
 
@@ -15,7 +16,7 @@ class AnakRepository {
     try {
       final response = await _serviceHttpClient.postWithToken(
         "admin/anak",
-        requestModel.toJson(),
+        requestModel.toJson()
       );
 
       if (response.statusCode == 201) {
