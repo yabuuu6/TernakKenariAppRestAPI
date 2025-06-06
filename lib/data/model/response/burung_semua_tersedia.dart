7d6584b7c465011e8f57bb4901735f0a1fafe73f
buyaramadhan <ytchochote@gmail.com>
2025-06-06 22:05:16 +0700

    menambahkan model DataBurungTersedia

--- lib/data/model/response/burung_semua_tersedia.dart
+++ lib/data/model/response/burung_semua_tersedia.dart
@@ -86,3 +86,78 @@ class BurungSemuaTersediabyIdModel {
   };
 }
 
+class DataBurungTersedia {
+  final int id;
+  final String image;
+  final String noRing;
+  final String jenisKelamin;
+  final String usia;
+  final String jenisKenari;
+  final int harga;
+  final String deskripsi;
+  final String status;
+
+  DataBurungTersedia({
+    required this.id,
+    required this.image,
+    required this.noRing,
+    required this.jenisKelamin,
+    required this.usia,
+    required this.jenisKenari,
+    required this.harga,
+    required this.deskripsi,
+    required this.status,
+  });
+
+  DataBurungTersedia copyWith({
+    int? id,
+    String? image,
+    String? noRing,
+    String? jenisKelamin,
+    String? usia,
+    String? jenisKenari,
+    int? harga,
+    String? deskripsi,
+    String? status,
+  }) => DataBurungTersedia(
+    id: id ?? this.id,
+    image: image ?? this.image,
+    noRing: noRing ?? this.noRing,
+    jenisKelamin: jenisKelamin ?? this.jenisKelamin,
+    usia: usia ?? this.usia,
+    jenisKenari: jenisKenari ?? this.jenisKenari,
+    harga: harga ?? this.harga,
+    deskripsi: deskripsi ?? this.deskripsi,
+    status: status ?? this.status,
+  );
+
+  factory DataBurungTersedia.fromRawJson(String str) =>
+      DataBurungTersedia.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory DataBurungTersedia.fromJson(Map<String, dynamic> json) =>
+      DataBurungTersedia(
+        id: json["id"],
+        image: json["image"],
+        noRing: json["no_ring"],
+        jenisKelamin: json["jenis_kelamin"],
+        usia: json["usia"],
+        jenisKenari: json["jenis_kenari"],
+        harga: json["harga"],
+        deskripsi: json["deskripsi"],
+        status: json["status"],
+      );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "image": image,
+    "no_ring": noRing,
+    "jenis_kelamin": jenisKelamin,
+    "usia": usia,
+    "jenis_kenari": jenisKenari,
+    "harga": harga,
+    "deskripsi": deskripsi,
+    "status": status,
+  };
+}
