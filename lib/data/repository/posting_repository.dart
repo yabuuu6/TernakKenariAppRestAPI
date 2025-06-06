6514f45b8922e7e7ab0355dba547ea1a5b3012fd
buyaramadhan <ytchochote@gmail.com>
2025-06-06 21:38:11 +0700

    menambahkan repository untuk posting burung

---
+++ lib/data/repository/posting_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:canary_template/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+
+class PostingRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  PostingRepository(this._serviceHttpClient);
+
+  Future<Either<String, BurungSemuaTersediabyIdModel>> addPostBurung(
+    PostingJualRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        'admin/posting-jual',
+        requestModel.toJson(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201) {
+        final profileResponse = BurungSemuaTersediabyIdModel.fromJson(
+          jsonResponse,
+        );
+        log("Add Burung successful: ${profileResponse.message}");
+        return Right(profileResponse);
+      } else {
+        log("Add burung failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Add burung failed");
+      }
+    } catch (e) {
+      log("Error in Add burung: $e");
+      return Left("An error occurred while post burung: $e");
+    }
+  }
+
+ 
+}
