2b53f93a57159e4599eddee932744f65deb82fc1
buyaramadhan <ytchochote@gmail.com>
2025-06-06 21:59:41 +0700

    menambahkan model GetAnak

--- lib/data/model/response/get_all_anak_response.dart
+++ lib/data/model/response/get_all_anak_response.dart
@@ -76,4 +76,72 @@ class GetAnakById {
   };
 }
 
+class GetAnak {
+  final int id;
+  final String noRing;
+  final String gambarBurung;
+  final DateTime tanggalLahir;
+  final String jenisKelamin;
+  final String jenisKenari;
+  final String ayahNoRing;
+  final String ibuNoRing;
 
+  GetAnak({
+    required this.id,
+    required this.noRing,
+    required this.gambarBurung,
+    required this.tanggalLahir,
+    required this.jenisKelamin,
+    required this.jenisKenari,
+    required this.ayahNoRing,
+    required this.ibuNoRing,
+  });
+
+  GetAnak copyWith({
+    int? id,
+    String? noRing,
+    String? gambarBurung,
+    DateTime? tanggalLahir,
+    String? jenisKelamin,
+    String? jenisKenari,
+    String? ayahNoRing,
+    String? ibuNoRing,
+  }) => GetAnak(
+    id: id ?? this.id,
+    noRing: noRing ?? this.noRing,
+    gambarBurung: gambarBurung ?? this.gambarBurung,
+    tanggalLahir: tanggalLahir ?? this.tanggalLahir,
+    jenisKelamin: jenisKelamin ?? this.jenisKelamin,
+    jenisKenari: jenisKenari ?? this.jenisKenari,
+    ayahNoRing: ayahNoRing ?? this.ayahNoRing,
+    ibuNoRing: ibuNoRing ?? this.ibuNoRing,
+  );
+
+  factory GetAnak.fromRawJson(String str) => GetAnak.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory GetAnak.fromJson(Map<String, dynamic> json) => GetAnak(
+    id: json["id"],
+    noRing: json["no_ring"],
+    gambarBurung:
+        json["gambar_burung"] ?? '',
+    tanggalLahir: DateTime.parse(json["tanggal_lahir"]),
+    jenisKelamin: json["jenis_kelamin"],
+    jenisKenari: json["jenis_kenari"],
+    ayahNoRing: json["ayah_no_ring"] ?? '', 
+    ibuNoRing: json["ibu_no_ring"] ?? '', 
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "no_ring": noRing,
+    "gambar_burung": gambarBurung,
+    "tanggal_lahir":
+        "${tanggalLahir.year.toString().padLeft(4, '0')}-${tanggalLahir.month.toString().padLeft(2, '0')}-${tanggalLahir.day.toString().padLeft(2, '0')}",
+    "jenis_kelamin": jenisKelamin,
+    "jenis_kenari": jenisKenari,
+    "ayah_no_ring": ayahNoRing,
+    "ibu_no_ring": ibuNoRing,
+  };
+}
