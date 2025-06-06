fe59d74305ba284d7c936a804b1b43bd391647c1
buyaramadhan <ytchochote@gmail.com>
2025-06-06 21:34:18 +0700

    menambahkan metode untuk mendapatkan semua data induk

--- lib/data/repository/induk_repository.dart
+++ lib/data/repository/induk_repository.dart
@@ -30,4 +30,20 @@ class IndukRepository {
     }
   }
 
+  Future<Either<String, GetAllIndukModel>> getAllInduk() async {
+    try {
+      final response = await _serviceHttpClient.get("admin/induk");
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetAllIndukModel.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while getting all induk: $e");
+    }
+  }
 }
