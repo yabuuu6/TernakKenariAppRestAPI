bb199af875233ee48cfa117a70d2124509423da5
buyaramadhan <ytchochote@gmail.com>
2025-06-06 21:44:24 +0700

    menambahkan repository untuk profil pembeli

---
+++ lib/data/repository/profile_buyer_repository.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:canary_template/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+
+class ProfileBuyerRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  ProfileBuyerRepository(this._serviceHttpClient);
+
+  Future<Either<String, BuyerProfileResponseModel>> addProfileBuyer(
+    BuyerProfileRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        "buyer/profile",
+        requestModel.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = BuyerProfileResponseModel.fromJson(
+          jsonResponse,
+        );
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
+
+
+}
